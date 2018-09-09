2710afc6fd2ed5d793f2fbb0a98e9f88899070be
paylm <paylm@paylm-pc.lan>
2018-09-09 10:28:15 +0800

    add utils.go

---
+++ utils.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"math/rand"
+	"time"
+)
+
+/**
+* size 随机码的位数
+* kind 0    // 纯数字
+       1    // 小写字母
+       2    // 大写字母
+       3    // 数字、大小写字母
+*/
+func krand(size int, kind int) []byte {
+	ikind, kinds, result := kind, [][]int{[]int{10, 48}, []int{26, 97}, []int{26, 65}}, make([]byte, size)
+	is_all := kind > 2 || kind < 0
+	rand.Seed(time.Now().UnixNano())
+	for i := 0; i < size; i++ {
+		if is_all { // random ikind
+			ikind = rand.Intn(3)
+		}
+		scope, base := kinds[ikind][0], kinds[ikind][1]
+		result[i] = uint8(base + rand.Intn(scope))
+	}
+	return result
+}
